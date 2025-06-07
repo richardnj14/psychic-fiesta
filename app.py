02b742d965922ad3c2cce1053c19cff251d810d0
Richard <richardnjanssen@gmail.com>
2025-06-07 14:10:37 +0200

    form moved to index file, index file placeholder added to base template, python app started.

---
+++ app.py
@@ -0,0 +1,31 @@
+#We need to import some flask package features
+from flask import Flask, render_template, request, redirect, url_for
+#Create a Flask app 
+app = Flask(__name__)
+
+#Create an empty array to store mood history, it will be reseted if app restarts
+mood_history = []
+
+#This is a decorator, it changes behavior of function that is defined just below this.
+#In this case, we tell python that the function who follows the decoratore needs to be executed when user open '/' path (home page)
+@app.route('/')
+def index():
+    #render_tempalte will generate a page using html specified html file
+    #it is also passing mood_history as 'history' variable in html, so we can show mood history
+    return render_template('index.html', history=mood_history)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    #when we define a form with the POST method in HTML, the submited data is inside a reques.form
+    #.get('mood') tells that we wand to get the value of 'mood' which is the 'name' of our selection in HTML
+    mood = request.form.get('mood')
+    #simply checks if mood variable is not null
+    if mood:
+        from datetime import datetime
+        timestamp = datetime.now().strftime('%Y-%m-%D %H:%M:%S')
+        mood_history.append(f"{timestamp}-{mood.capitalize()}")
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
